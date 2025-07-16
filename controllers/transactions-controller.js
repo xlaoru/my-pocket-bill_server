7c5fac1d26213ad8063e62be83cb4ff442ba64e5
xlaoru <sia212007@gmail.com>
2025-07-16 22:17:22 +0300

    data-unloading-logic-creation

--- controllers/transactions-controller.js
+++ controllers/transactions-controller.js
@@ -1,11 +1,29 @@
 const Transaction = require("../models/transaction.model.js");
 
-exports.getTransactions = async function  (req, res) {
+exports.getTransactions = async function (req, res) {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
+    const startDate = req.query.startDate
+    const endDate = req.query.endDate;
     const type = req.query.type;
 
-    const filter = type ? { type } : {};
+    const filter = {};
+
+    if (type) {
+        filter.type = type;
+    }
+
+    if (startDate || endDate) {
+        filter.date = {}
+
+        if (startDate) {
+            filter.date.$gte = new Date(new Date(startDate).setHours(0, 0, 0, 0));
+        }
+
+        if (endDate) {
+            filter.date.$lte = new Date(new Date(endDate).setHours(23, 59, 59, 999));
+        }
+    }
 
     try {
         const total = await Transaction.countDocuments(filter);
@@ -19,9 +37,31 @@ exports.getTransactions = async function  (req, res) {
             transactions,
             total,
             page,
-            pages: Math.ceil(total / limit)
+            pages: Math.ceil(total / limit),
+            message: "Transactions retrieved successfully"
         });
-    } catch (err) {
+    } catch (error) {
         res.status(500).json({ error: "Server error" });
     }
 };
+
+exports.getFinanceCategoriesSummary = async function (req, res) {
+    try {
+        const myBalance = 0
+
+        const monthlyExpenses = 0
+
+        const myInvestments = 0
+
+        const myReserve = 0
+
+        res.status(200).json({
+            myBalance,
+            monthlyExpenses,
+            myInvestments,
+            myReserve
+        })
+    } catch(error) {
+        res.status(500).json({ error: "Server error" });
+    }
+}
--- routes/transaction-route.js
+++ routes/transaction-route.js
@@ -3,8 +3,11 @@ const router = new Router();
 
 const {
     getTransactions,
+    getFinanceCategoriesSummary
 } = require("../controllers/transactions-controller.js");
 
 router.get("/transactions", getTransactions);
+
+router.get("/transactions/finance-categories-summary", getFinanceCategoriesSummary)
 
 module.exports = router;
